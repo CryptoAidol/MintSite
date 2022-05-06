2903440d4f46a92a8b922771cb55519848ebffd4
{Mameta29} <{fc.kikkawa@gmail.com}>
2022-05-06 21:39:53 +0900

    fs kono

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -45,20 +45,3 @@ import cryptoaidolLogo from '../assets/cryptoaidolLogo.png'
     )
   }
    export default Footer;
-
-//  function Footer() {
-//      return       <footer className="bg-slate-500">
-//      <div className="grid grid-cols-12 gap-12 place-content-start">
-//        <div></div>
-//        <div className='text-xl bg-slate-500 px-1 py-1 text-slate-100'>Crypto Aidol</div>
-//      </div>
-//      <div className='grid grid-cols-4 lg:grid lg:grid-cols-12 lg:gap-12'>
-//        <div></div>
-//        <div className='px-1 pb-2 text-slate-100 '><a href="/">Document</a></div>
-//        <div className='px-3 pb-2 text-slate-100 '><a href="https://discord.gg/wVeXbdyB">Discord</a></div>
-//        <div className='px-4 pb-2 text-slate-100 '><a href="https://twitter.com/crypto111977836">Twitter</a></div>
-//      </div>
-//    </footer>;
-//    }
-
-//    export default Footer;
---
+++ frontend/components/MintBottun.tsx
@@ -0,0 +1,42 @@
+import mintNFT from '../../scripts/mint-nft.js'
+import Link from 'next/link'
+import {useState} from 'react'
+
+const style = {
+    headerIcon: `flex items-center justify-end bg-[#f797e7] text-white text-xl px-4 font-black rounded-full shadow-xl hover:bg-[#fcdef7] hover:text-[#f797e7] cursor-pointer`,
+  }
+
+function MintBottun () {
+    const [walletAddress, setWalletAddress] = useState("")
+    async function requestAccount() {
+        console.log('Requesting account...')
+    
+        // Check if Meta Mask Extension exists 
+        if(window.ethereum) {
+          console.log('detected')
+    
+          try {
+            const accounts = await window.ethereum.request({
+              method: "eth_requestAccounts",
+            });
+            setWalletAddress(accounts[0])
+            mintNFT("ipfs://QmQcxqVUJoefCzAKJ1iFneGu4NUa7jJ3gjqsSmJJXb7WQc")
+          } catch (error) {
+            console.log('Error connecting')
+          }
+        } else {
+          alert('Meta Mask not detected')
+        }
+      }
+    
+    return (
+          <Link href="/">
+            <div className={style.headerIcon}>
+              <button onClick={requestAccount}>
+                mint!</button>
+            </div>
+          </Link>
+    )
+}
+
+export default MintBottun;
